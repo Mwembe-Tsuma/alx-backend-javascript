f76a001b153bb7a09e12b62fb71c9336690fbaf1
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-01-05 09:50:00 +0000

    5-photo-reject.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,12 +1,5 @@
 export default function uploadPhoto(filename) {
-  return new Promise((resolve, reject) => {
-    const isError = true; // Set to true to simulate an error
-
-    if (isError) {
-      reject(new Error(`Error: ${filename} cannot be processed`));
-    } else {
-      const processedData = `Processed data for ${filename}`;
-      resolve(processedData);
-    }
-  });
+  return Promise.reject(
+    new Error(`${filename} cannot be processed`),
+  );
 }
