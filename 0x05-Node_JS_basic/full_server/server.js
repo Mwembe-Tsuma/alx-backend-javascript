2e85d05ce8492e1425fdaad8bc251da909737cbe
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-27 09:42:06 +0000

    utils.js

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,16 @@
+// File: full_server/server.js
+
+import express from 'express';
+import mapRoutes from './routes';
+
+const app = express();
+const PORT = 1245;
+
+mapRoutes(app);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+export default app;
+module.exports = app;
