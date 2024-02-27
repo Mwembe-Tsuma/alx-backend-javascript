3a5a03364f058930fea1daf71eb2733f6d6ea860
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-27 06:21:35 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,11 @@
 // File: 1-stdin.js
 
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-console.log("Welcome to Holberton School, what is your name?");
-
-rl.on('line', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
-
-rl.on('close', () => {
-  console.log("This important software is now closing");
-});
-
