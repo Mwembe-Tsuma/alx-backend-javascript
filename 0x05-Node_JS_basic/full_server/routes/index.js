22ecbe42913a415bd736eba23c111d421ac1eb93
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-27 09:59:14 +0000

    Routes

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,13 @@
+// File: full_server/routes/index.js
+
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
