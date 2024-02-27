5652519e06bf0b775f8ef2060f0c4b6c51e0f561
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-27 09:56:10 +0000

    controllers

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,10 @@
+// File: full_server/controllers/AppController.js
+
+class AppController {
+  static getHomepage(request, res) {
+    res.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
