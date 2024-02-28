2889c880b68d7f91f9921e654353725224b33264
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-28 09:45:23 +0000

    Basic test with Mocha

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,5 @@
+// File: 0-calcul.js
+
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/run-calcul.js
@@ -0,0 +1,9 @@
+// File: run-calcul.js
+
+const calculateNumber = require('./0-calcul.js');
+
+// Example usage:
+console.log(calculateNumber(1, 3));     // Output: 4
+console.log(calculateNumber(1, 3.7));   // Output: 5
+console.log(calculateNumber(1.2, 3.7)); // Output: 5
+console.log(calculateNumber(1.5, 3.7)); // Output: 6
