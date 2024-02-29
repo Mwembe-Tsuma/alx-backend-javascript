2312d71749dc0ad8290cf0b1a2175bffc4365dfc
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-29 11:12:59 +0000

    3-payment.test.js

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,19 @@
+// File: 3-payment.test.js
+
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber with type SUM', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnce).to.be.true;
+    expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+});
